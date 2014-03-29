b9beb115ecdd2a104c7708508f9479dcdff6d0bd
Jeffrey Doto <jeff_doto@cable.comcast.com>
2014-03-29 16:28:51 -0400

    deletes junk

--- search_lyrics.py
+++
@@ -1,9 +0,0 @@
-import lyrics
-import sys
-
-# Simple wrapper around lyrics.py, call like so: python lyrics.py 'Artist' 'Song' to print lyrics for the song, and write a file to disk named '<artist>-<song>-lyrics.txt'
-
-artist = sys.argv[1]
-song = sys.argv[2]
-
-lyrics.searchAndWriteFile(artist, song)
