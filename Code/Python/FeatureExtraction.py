8784c7f7e2499b5c7d1f3e148c4fa6aedf537714
Brandon <bmorton1@gmail.com>
2014-03-30 12:21:45 -0400

    Vocal extraction code updated and pickle file with data created

--- Code/Python/FeatureExtraction.py
+++ Code/Python/FeatureExtraction.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
 import pdb
-from os.path import isdir, join, isfile
 import numpy as np
 import librosa
+
+from cPickle import dump
+from os.path import isdir, join, isfile
 from MP3IO import MP3Handler
 from scipy.io import loadmat
 
 def plotArray(data):
+	"""
+	plotArray() - Helper function for plotting arrays
+
+		Inputs:
+			data - data to be plotted
+
+		Outputs:
+			None 
+	"""
+
 	fig1 = plt.figure()
 	ax = fig1.add_subplot(111)
 	ax.plot(data)
@@ -14,7 +26,7 @@ def plotArray(data):
 
 def contrast(spec, sr, a):
 	""" 
-	Compute spectral contrast
+	contrast() - Compute spectral contrast
 
 		Inputs: 
 			spec - spectrogram
@@ -36,8 +48,8 @@ def contrast(spec, sr, a):
 		sortedFreqs = np.sort(spec[:,i])
 		nbinsToLook = np.round(nbins * a).astype("int")
 
-		valley = np.log(np.sum(sortedFreqs[:nbinsToLook])) / nbinsToLook
-		peak = np.log(np.sum(sortedFreqs[nbins-nbinsToLook:nbins])) / nbinsToLook
+		valley = np.log(np.sum(sortedFreqs[:nbinsToLook]) + np.spacing(1)) / nbinsToLook
+		peak = np.log(np.sum(sortedFreqs[nbins-nbinsToLook:nbins]) + np.spacing(1)) / nbinsToLook
 
 		contrasts[i] = peak - valley
 
@@ -45,7 +57,7 @@ def contrast(spec, sr, a):
 
 def centroid(spec, sr):
 	""" 
-	Compute spectral centroid
+	centroid() - Compute spectral centroid
 
 		Inputs: 
 			spec - spectrogram
@@ -64,44 +76,68 @@ def centroid(spec, sr):
 	centroids = np.zeros(nframes)
 	for i in range(nframes):
 		centerFreqs = np.multiply(freqRes, range(nbins))
-		centroids[i] = np.sum(np.multiply(spec[:,i],centerFreqs)) / np.sum(spec[:,i])
+		centroids[i] = np.sum(np.multiply(spec[:,i],centerFreqs)) / (np.sum(spec[:,i]) + np.spacing(1))
 
 	return centroids
 
+def clustering(pickleFile, ):
+
 def main():
+	""" 
+	main() - Main function for feature extraction
+
+		Inputs: 
+			None
+
+		Outputs:
+			Pickle file with feature data
+	"""
 
 	vocalData = loadmat('../../Data/firstVerseTimes.mat')
 	audioPath = '../../Audio/Vocals/'
+	assert isdir(audioPath), "Audio path does not exist"		# Make sure directory of audio exists
 
-	# Make sure directory of audio exists
-	assert isdir(audioPath), "Audio path does not exist"
-	fileList = librosa.util.find_files(audioPath, ext='mp3')
+	fileList = [ join(audioPath, 'Vocals_' + str(vocalData['firstVerseTimes'][i][3][0])) for i in range(len(vocalData['firstVerseTimes'])) ]
 	numFiles = len(fileList)
-	vocalFeatures = np.zeros((numFiles, 3))
+	vocalFeatures = np.zeros((numFiles, 8))
 
 	for i in range(numFiles):
 
+		print 'Working on file {} of {}'.format(i, numFiles)
 		# Read in audio 
 		audio, sr = librosa.load(fileList[i], sr=44100)
 		S = librosa.stft(audio, n_fft = 1024, hop_length = 512)
 		spec = np.abs(S)
 
-		centroids = centroid(spec, sr)
-		contrasts = contrast(spec, sr, 0.05)
-		pdb.set_trace()
+		# Extract features
+		centroids = centroid(spec, sr)														# Spectral centroid
+		contrasts = contrast(spec, sr, 0.05)												# Spectral contrast
+		onset_frames    = librosa.onset.onset_detect(y=audio, sr=sr, hop_length=64)			# Calculate frames of onsets
+		onset_times     = librosa.frames_to_time(onset_frames, sr, hop_length=64)			# Calculate times of onsets
 
-		# Calculate locations of onsets
-		onset_frames    = librosa.onset.onset_detect(y=audio, sr=sr, hop_length=64)
-		onset_times     = librosa.frames_to_time(onset_frames, sr, hop_length=64)
+		# Extract feature statistics
+		vocalFeatures[i,0] =  np.mean(np.diff(onset_times))						# Mean of onset durations
+		vocalFeatures[i,1] = np.var(np.diff(onset_times))						# Variance of onset durations
+		vocalFeatures[i,2], beats = librosa.beat.beat_track(audio, sr )			# Get beats and tempo
+		vocalFeatures[i,3] = np.mean(centroids)									# Mean of centroids
+		vocalFeatures[i,4] = np.var(centroids)									# Variance of centroids
+		vocalFeatures[i,5] = np.mean(contrasts)									# Mean of spectral contrast
+		vocalFeatures[i,6] = np.var(contrasts)									# Mean of spectral contrast
+		vocalFeatures[i,7] = onset_times.shape[0] / (audio.shape[0] / float(sr))# Onset density
 
-		#Find average IOI and variance
-		vocalFeatures[i,1] =  np.mean(np.diff(onset_times))						# Mean of onset durations
-		vocalFeatures[i,2] = np.var(np.diff(onset_times))						# Variance of onset durations
-		vocalFeatures[i,3], beats = librosa.beat.beat_track(audio, sr )			# Get beats and tempo
-		vocalFeatures[i,4] = np.mean(centroids)									# Mean of centroids
-		vocalFeatures[i,5] = np.var(centroids)									# Variance of centroids
-		vocalFeatures[i,6] = np.mean(contrasts)									# Mean of spectral contrast
-		vocalFeatures[i,7] = np.var(contrasts)									# Mean of spectral contrast
+	# Create dictionary for features
+	dataDict = {'ID': vocalData['firstVerseTimes'][:][0][0][0], 
+				'onsetMean': vocalFeatures[:,0],
+				'onsetVar': vocalFeatures[:,1],
+				'tempo': vocalFeatures[:,2],
+				'centroidMean': vocalFeatures[:,3],
+				'centroidVar': vocalFeatures[:,4],
+				'contrastMean': vocalFeatures[:,5],
+				'contrastVar': vocalFeatures[:,6],
+				'onsetDensity': vocalFeatures[:,7]}
 
+	dump(dataDict, open('vocalFeatureData.p', 'w'))
+
+	print ('Done')
 if __name__ == '__main__':
 	main()
